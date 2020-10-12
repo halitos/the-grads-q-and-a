a7ee896f75e97717ca77516987147178d9d73c91
Halit <60817499+halitos@users.noreply.github.com>
2020-10-12 15:07:17 +0100

    pushing all

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,37 @@
 import React, { useEffect, useState } from "react";
+import List from "../Components/List";
+import AskQuestion from "../Components/AskQuestion";
 
 import "./App.css";
-import { getMessage } from "./service";
-import logo from "./logo.svg";
+// import { getMessage } from "./service";
+// import logo from "./logo.svg";
 
-export function App() {
-	const [message, setMessage] = useState("Loading...");
+// export function App() {
+// 	const [message, setMessage] = useState("Loading...");
 
-	useEffect(() => {
-		getMessage().then((message) => setMessage(message));
-	}, []);
+// 	useEffect(() => {
+// 		getMessage().then((message) => setMessage(message));
+// 	}, []);
 
+// 	return (
+// 		<main role="main">
+// 			<div>
+// 				<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
+// 				<h1 className="message" data-qa="message">{message} local mesage</h1>
+// 			</div>
+// 		</main>
+// 	);
+// }
+
+function App() {
 	return (
-		<main role="main">
-			<div>
-				<img className="logo" data-qa="logo" src={logo} alt="Just the React logo" />
-				<h1 className="message" data-qa="message">{message}</h1>
-			</div>
-		</main>
+	  <div className="App">
+		<div className="container">
+		  <AskQuestion />
+		  <List />
+		</div>
+	  </div>
 	);
-}
+  }
 
 export default App;
